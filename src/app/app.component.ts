678c14cfe04e5e5b07b1f4c26c76d9867853702e
Ahmed Galadi <galadiahmed99@gmail.com>
2021-09-03 03:21:30 +0100

    setting up firebase and the authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'booksLibrary';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAUhBkltu758l76kvAnTS8WwFBl5bmhWag",
+      authDomain: "books-library-13d17.firebaseapp.com",
+      projectId: "books-library-13d17",
+      storageBucket: "books-library-13d17.appspot.com",
+      messagingSenderId: "434906818178",
+      appId: "1:434906818178:web:f6b4262ab006c417d1f69b",
+      measurementId: "G-K9SCBJXJ7E"
+    };
+
+// Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,40 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  creatNewUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth , email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signInUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signOut() {
+    getAuth().signOut();
+  }
 }
